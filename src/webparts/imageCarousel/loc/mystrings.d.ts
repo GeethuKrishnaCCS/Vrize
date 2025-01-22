3412e77a4e892c4651d40c29c093bb293d7f02fb
Geethu Krishna <geethu.krishna@ccsdev01.onmicrosoft.com>
2025-01-22 11:18:31 +0530

    initial commit

---
+++ src/webparts/imageCarousel/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IImageCarouselWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ImageCarouselWebPartStrings' {
+  const strings: IImageCarouselWebPartStrings;
+  export = strings;
+}
