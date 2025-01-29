67775c272a5220d4d83fd7388b325b91a003c149
rafadCCS <rafad.aranhikkal@ccs-technologies.com>
2025-01-29 10:33:20 +0530

    Mission-Vision - Static

--- src/webparts/missionVision/MissionVisionWebPart.ts
+++ src/webparts/missionVision/MissionVisionWebPart.ts
@@ -3,17 +3,22 @@ import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
 import {
   type IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneLabel,
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
 
 import * as strings from 'MissionVisionWebPartStrings';
-import MissionVision from './components/MissionVision';
-import { IMissionVisionProps } from './components/IMissionVisionProps';
+import MissionVision, { IMissionVisionProps } from './components/Main';
+import { PropertyFieldMonacoEditor } from '@pnp/spfx-property-controls';
+import { ICoreValuesConfig, IMissionConfig, IStoryConfig, IVisionConfig } from './types/Types';
+// import { generateJsonSchema } from './utils/generateSchema';
 
 export interface IMissionVisionWebPartProps {
-  description: string;
+  storyConfig: string;
+  missionConfig: string;
+  visionConfig: string;
+  coreValuesConfig: string;
 }
 
 export default class MissionVisionWebPart extends BaseClientSideWebPart<IMissionVisionWebPartProps> {
@@ -25,11 +30,14 @@ export default class MissionVisionWebPart extends BaseClientSideWebPart<IMission
     const element: React.ReactElement<IMissionVisionProps> = React.createElement(
       MissionVision,
       {
-        description: this.properties.description,
         isDarkTheme: this._isDarkTheme,
         environmentMessage: this._environmentMessage,
         hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        userDisplayName: this.context.pageContext.user.displayName,
+        storyConfig: this.parseConfig<IStoryConfig>(this.properties.storyConfig),
+        missionConfig: this.parseConfig<IMissionConfig>(this.properties.missionConfig),
+        visionConfig: this.parseConfig<IVisionConfig>(this.properties.visionConfig),
+        coreValuesConfig: this.parseConfig<ICoreValuesConfig>(this.properties.coreValuesConfig),
       }
     );
 
@@ -42,7 +50,17 @@ export default class MissionVisionWebPart extends BaseClientSideWebPart<IMission
     });
   }
 
-
+  private parseConfig<T>(config: string | T | undefined): T {
+    try {
+      if (typeof config === "string") {
+        return JSON.parse(config) as T;
+      }
+      return (config || undefined) as T; // Return the object or undefined
+    } catch (error) {
+      console.warn("Failed to parse config. Returning default value.", error);
+      return undefined as unknown as T; // Fallback to undefined
+    }
+  }
 
   private _getEnvironmentMessage(): Promise<string> {
     if (!!this.context.sdks.microsoftTeams) { // running in Teams, office.com or Outlook
@@ -96,6 +114,53 @@ export default class MissionVisionWebPart extends BaseClientSideWebPart<IMission
   protected get dataVersion(): Version {
     return Version.parse('1.0');
   }
+  // private schema = generateJsonSchema("./types/Types.ts", "IStoryConfig")
+  // private schema = {
+  //   "$schema": "http://json-schema.org/draft-07/schema#",
+  //   "type": "object",
+  //   "properties": {
+  //     "title": {
+  //       "type": "string",
+  //       "description": "Title of the story"
+  //     },
+  //     "description": {
+  //       "type": "string",
+  //       "description": "Description of the story"
+  //     },
+  //     "author": {
+  //       "type": "string",
+  //       "description": "Author of the story"
+  //     },
+  //     "publishedDate": {
+  //       "type": "string",
+  //       "format": "date-time",
+  //       "description": "Date when the story was published"
+  //     },
+  //     "tags": {
+  //       "type": "array",
+  //       "items": {
+  //         "type": "string"
+  //       },
+  //       "description": "Tags associated with the story"
+  //     },
+  //     "metadata": {
+  //       "type": "object",
+  //       "properties": {
+  //         "version": {
+  //           "type": "number",
+  //           "description": "Version of the story"
+  //         },
+  //         "lastModified": {
+  //           "type": "string",
+  //           "format": "date-time",
+  //           "description": "Last modified date"
+  //         }
+  //       },
+  //       "required": ["version", "lastModified"]
+  //     }
+  //   },
+  //   "required": ["title", "description", "publishedDate", "tags", "metadata"]
+  // }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
@@ -108,9 +173,69 @@ export default class MissionVisionWebPart extends BaseClientSideWebPart<IMission
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
+                PropertyPaneLabel('storyConfigJSONField', {
+                  text: "Story Config (JSON)"
+                }),
+                PropertyFieldMonacoEditor('storyConfig', {
+                  key: 'storyConfigJSONField',
+                  // value: this.properties.StatusMappingJSON,
+                  value: typeof this.properties.storyConfig === "string" ? this.properties.storyConfig : JSON.stringify(this.properties.storyConfig, null, 2),
+                  language: "json",
+                  showLineNumbers: true,
+                  jsonDiagnosticsOptions: {
+                    allowComments: true,
+                    validate: true,
+                    schemaValidation: 'error',
+                    // schemas: [
+                    //   {
+                    //     uri: 'storyConfigSchema',
+                    //     fileMatch: ['*'],
+                    //   }
+                    // ]
+                  }
+                }),
+                PropertyPaneLabel('missionConfigJSONField', {
+                  text: "Mission Config (JSON)"
+                }),
+                PropertyFieldMonacoEditor('missionConfig', {
+                  key: 'missionConfigJSONField',
+                  value: typeof this.properties.missionConfig === "string" ? this.properties.missionConfig : JSON.stringify(this.properties.missionConfig, null, 2),
+                  language: "json",
+                  showLineNumbers: true,
+                  jsonDiagnosticsOptions: {
+                    allowComments: true,
+                    validate: true,
+                    schemaValidation: 'error'
+                  }
+                }),
+                PropertyPaneLabel('visionConfigJSONField', {
+                  text: "Mission Config (JSON)"
+                }),
+                PropertyFieldMonacoEditor('visionConfig', {
+                  key: 'missionConfigJSONField',
+                  value: typeof this.properties.visionConfig === "string" ? this.properties.visionConfig : JSON.stringify(this.properties.visionConfig, null, 2),
+                  language: "json",
+                  showLineNumbers: true,
+                  jsonDiagnosticsOptions: {
+                    allowComments: true,
+                    validate: true,
+                    schemaValidation: 'error'
+                  }
+                }),
+                PropertyPaneLabel('coreValuesConfigJSONField', {
+                  text: "Core Values Config (JSON)"
+                }),
+                PropertyFieldMonacoEditor('coreValuesConfig', {
+                  key: 'coreValuesConfigJSONField',
+                  value: typeof this.properties.coreValuesConfig === "string" ? this.properties.coreValuesConfig : JSON.stringify(this.properties.coreValuesConfig, null, 2),
+                  language: "json",
+                  showLineNumbers: true,
+                  jsonDiagnosticsOptions: {
+                    allowComments: true,
+                    validate: true,
+                    schemaValidation: 'error'
+                  }
+                }),
               ]
             }
           ]
--- src/webparts/missionVision/components/IMissionVisionProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IMissionVisionProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
