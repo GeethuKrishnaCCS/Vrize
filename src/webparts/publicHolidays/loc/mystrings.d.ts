2907be64ba41b999eb85320d5943c4cacca2982b
Geethu Krishna <geethu.krishna@ccsdev01.onmicrosoft.com>
2025-01-30 19:27:54 +0530

    Public Holiday

---
+++ src/webparts/publicHolidays/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IPublicHolidaysWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'PublicHolidaysWebPartStrings' {
+  const strings: IPublicHolidaysWebPartStrings;
+  export = strings;
+}
