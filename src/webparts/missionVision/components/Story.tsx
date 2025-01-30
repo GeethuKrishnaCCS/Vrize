d4934676fbe3d89867dd79e218f531483bf5fc53
Geethu Krishna <geethu.krishna@ccsdev01.onmicrosoft.com>
2025-01-30 19:23:35 +0530

    mission vision

---
+++ src/webparts/missionVision/components/Story.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import styles from './Story.module.scss';
+import { IStoryConfig } from '../types/Types';
+
+export interface IStoryProps {
+    config: IStoryConfig;
+}
+
+export const Story: React.FunctionComponent<IStoryProps> = (props: React.PropsWithChildren<IStoryProps>) => {
+    const storyData = props.config
+
+    if (!storyData) {
+        return null;
+    }
+
+    return (
+        <section className={styles.storySection}>
+            <div className={styles.imageContainer}>
+                <img
+                    src={storyData.image.src}
+                    alt={storyData.image.alt}
+                    className={styles.storyImage}
+                />
+            </div>
+            <article className={styles.textContent}>
+                <h2 className={styles.storyTitle}>{storyData.title}</h2>
+                {storyData.paragraphs.map((paragraph: string, index: number) => (
+                    <p key={index} className={styles.storyParagraph}
+                        dangerouslySetInnerHTML={{ __html: paragraph }} />
+                ))}
+            </article>
+        </section>
+    );
+
+};
