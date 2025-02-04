19686fe922435f46f9088d0182d36ef6df12135c
Geethu Krishna <geethu.krishna@ccsdev01.onmicrosoft.com>
2025-02-04 13:12:05 +0530

    My Teams

---
+++ src/webparts/myTeams/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IMyTeamsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'MyTeamsWebPartStrings' {
+  const strings: IMyTeamsWebPartStrings;
+  export = strings;
+}
